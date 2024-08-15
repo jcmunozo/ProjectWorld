dc05953e8c8e2bddc02f1b46e69e2214d9d1043e
jcmunozo <jcmunozo@unicauca.edu.co>
2024-08-15 16:44:13 -0500

    Added .gitignore and logic to country

--- src/Modelo/ClsPais.java
+++ src/Modelo/ClsPais.java
@@ -10,5 +10,78 @@ package Modelo;
  * @author Camilo
  */
 public class ClsPais {
+	private String id;
+    private String nombre;
+    private String continente;
+    private String idioma;
+    private String moneda;
+    private String bandera;
+    private String capital;
+    private int poblacion;
     
+    public ClsPais() {
+    }
+
+	public ClsPais(String id, String nombre, String continente, String idioma, String moneda, String bandera,
+			String capital, int poblacion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.continente = continente;
+		this.idioma = idioma;
+		this.moneda = moneda;
+		this.bandera = bandera;
+		this.capital = capital;
+		this.poblacion = poblacion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContinente() {
+		return continente;
+	}
+	public void setContinente(String continente) {
+		this.continente = continente;
+	}
+	public String getIdioma() {
+		return idioma;
+	}
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	public String getMoneda() {
+		return moneda;
+	}
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+	public String getBandera() {
+		return bandera;
+	}
+	public void setBandera(String bandera) {
+		this.bandera = bandera;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public int getPoblacion() {
+		return poblacion;
+	}
+	public void setPoblacion(int poblacion) {
+		this.poblacion = poblacion;
+	}
+	public String getId() {
+		return id;
+	}
+	
+    public String getFullInfo() {
+        return nombre + " capital: " + capital + ", con una poblacion de: " + poblacion + " habitantes" ;
+    }
 }
--- src/proyectoworld/ProyectoWorld.java
+++ src/proyectoworld/ProyectoWorld.java
@@ -5,6 +5,11 @@
  */
 package proyectoworld;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Modelo.ClsPais;
+
 /**
  *
  * @author Camilo
@@ -15,7 +20,22 @@ public class ProyectoWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	List<ClsPais> paises = new ArrayList<>();
+    	ClsPais colombia = new ClsPais("1", "Colombia", "America", "Español", "peso colombiano", "url_to_image", "Bogota", 50_000_000);
+    	ClsPais francia = new ClsPais("2", "Francia", "europa", "frances", "euro", "url_to_image", "Paris", 68_401_997);
+    	ClsPais ecuador = new ClsPais("3", "Ecuador", "America", "Español", "dolar", "url_to_image", "Quito", 17_483_326);
+    	ClsPais venezuela = new ClsPais("4", "Venezuela", "America", "Español", "bolivar", "url_to_image", "Caracas", 30_518_260);
+    	ClsPais canada = new ClsPais("5", "Canada", "America", "Frances/Ingles", "dolar canadiense", "url_to_image", "Ottawa", 41_012_563);
+    	ClsPais inglaterra = new ClsPais("6", "Inglaterra", "Europa", "Ingles", "libra esterlina", "url_to_image", "Londres", 67_596_281);
+    	ClsPais noruega = new ClsPais("7", "Noruega", "Europa", "Noruego", "corona noruega", "url_to_image", "Oslo", 5_550_203);
+    	paises.add(colombia);
+    	paises.add(francia);
+    	paises.add(ecuador);
+    	paises.add(venezuela);
+    	paises.add(canada);
+    	paises.add(inglaterra);
+    	paises.add(noruega);
+    	paises.stream().forEach(pais -> System.out.println(pais.getFullInfo()));  	
     }
     
 }
